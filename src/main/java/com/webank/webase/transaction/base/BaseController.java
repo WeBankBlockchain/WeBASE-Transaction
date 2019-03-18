3f4da2e40ed17feef197425fc55188dfb4da3975
lgw_gary <lgw_gary@10.39.182.201>
2019-03-18 19:47:58 +0800

    code check style

--- src/main/java/com/webank/webase/transaction/base/BaseController.java
+++ src/main/java/com/webank/webase/transaction/base/BaseController.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.webank.webase.transaction.base;
 
 import com.alibaba.fastjson.JSON;
